934127020a575d564b2b5af2da15c593b1ada75a
taeseokyoung <xotjrud@gmail.com>
2022-10-06 17:42:00 +0900

    20221006

--- src/App.js
+++ src/App.js
@@ -1,15 +1,71 @@
-// GET /v3/search/book HTTP/1.1
-// Host: dapi.kakao.com
-// Authorization: KakaoAK ${REST_API_KEY}
+import React, { useRef, useState } from 'react'
 
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+const App = () => {
+    const [input, setInput] = useState({});
+    const [boardList, setBoardList] = useState([]);
+    const id = useRef(1)
+    const todoHandler = e => {
+        setInput({
+            ...input,
+            [e.target.name]: e.target.value,
+            id: id.current
+        })
+    }
 
-    </div>
-  );
+    const commitHandler = () => {
+        setBoardList([...boardList,
+            input
+        ])
+        id.current++
+        setInput({
+            title: "",
+            content: "",
+        })
+    }
+
+    return (
+
+        <div className='Wrap'>
+            <header>
+                <div className="box">
+                    <h1>MYDATA</h1>
+
+                </div>
+            </header>
+            <div className="inner">
+                <section className='LifeQuotes'>
+                    {/* <slider>
+                        <div>“Talk is cheap. Show me the code.”</div>
+                        <div>“프로그래밍은 자기 얼굴을 차는 것과 같아서, 조만간 코피가 날 것이다.”</div>
+                    </slider> */}
+                </section>
+
+                <section className='Enter'>
+                    <input name='title' onChange={todoHandler} value={input.title || ""} />
+                    {/* <input name='content' onChange={todoHandler} value={input.content || ""} /> */}
+                    <button onClick={commitHandler}>Commit</button>
+                </section>
+                <section className="TodoList">
+                    <div className="box">
+                        {console.log(input)}
+                        <ul>
+                            {
+                                boardList.map((it, idx) => {
+                                    return (
+                                        <li key={it.id}>{it.title}</li>
+                                    )
+                                }).reverse()
+                            }
+                        </ul>
+                    </div>
+                </section>
+            </div>
+        </div>
+    )
 }
 
 export default App;
+
+
+// https://velog.io/@e_juhee/Github-%EC%9E%94%EB%94%94-%EB%B3%B4%EC%97%AC%EC%A3%BC%EA%B8%B0
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
+import './common.css'
+import './App.scss'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
